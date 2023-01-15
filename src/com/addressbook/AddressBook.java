b6578a6fef0245e7c1b9db6122e4bddeafc2665d
PrathmPatil <prathmpatil2818@gmail.com>
2023-01-16 00:29:24 +0530

    Add Contact in AddressBook

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -3,8 +3,44 @@ package com.addressbook;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
-
 public class AddressBook {
+	/*
+	 * Create arraylist of contactinfo class 
+	 * take info and store in array
+	 * using add method take inputfrom user and tore in array
+	 * then display the contact
+	 */
 	ArrayList<ContactInfo> contact=new ArrayList<ContactInfo>();
+
+public void addContact() {
+	 ContactInfo contactinfo=new ContactInfo();
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("Enter first name: ");
+	contactinfo.setFirst(sc.nextLine());
+	System.out.println("Enter last name: ");
+	contactinfo.setLast(sc.nextLine());
+	System.out.println("Enter Address: ");
+	contactinfo.setAddress(sc.nextLine());
+	System.out.println("Enter city: ");
+	contactinfo.setCity(sc.nextLine());
+	System.out.println("Enter state: ");
+	contactinfo.setState(sc.nextLine());
+	System.out.println("Enter zip code: ");
+	contactinfo.setZip(sc.nextLine());
+	System.out.println("Enter Email: ");
+	contactinfo.setEmail(sc.nextLine());
+	System.out.println("Enter phone number: ");
+	contactinfo.setPhoneNumber(sc.nextLine());
+	contact.add(contactinfo);
+}
+public void displayContact()
+{ 
+     contact.forEach((info)->{
+   	  System.out.println("Name: "+info.getFirst()+" "+info.getLast());
+   	  System.out.println("Address: "+info.getAddress()+"\n"+"City: "+info.getCity());
+   	  System.out.println("State: "+info.getState()+"\n"+"Zip: "+info.getZip()+"\n"+"Email: "+info.getEmail());
+   	  System.out.println("Phone Number: "+info.getPhoneNumber());
+   	  
+     });
+}
 }
--- src/com/addressbook/Main.java
+++ src/com/addressbook/Main.java
@@ -4,9 +4,32 @@ import java.util.Scanner;
 
 public class Main {
 	
+	/*
+	 * create obj of AddressBook class 
+	 * create choise method and take working condition from user
+	 * using while loop recurcivelly call t choise method;
+	 */
+AddressBook addressbook=new AddressBook();
+        public void choise()
+    {   
+	System.out.println("************Your Address Book***************");
+	System.out.println("Press 1 for add contact: ");
+	int choose=(new Scanner(System.in).nextInt());
+	switch(choose)
+	 {
+	case 1: addressbook.addContact();
+	        System.out.println();
+	        addressbook.displayContact();
+	        System.out.println();
+	 }
+	choise();
+   }
 	public static void main(String[] args) {
 		System.out.println("Welcme in AddressBook");
-		
-	}
+
+             Main main=new Main();
+             main.choise();
+	
+   	}
 
 }
